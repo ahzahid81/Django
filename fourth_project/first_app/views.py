78e2d7a0b2bf7df668c7ad667bcd1b1c5b0d49e2
Abdul Hasib Zahid <abdulhasibzahid@gmail.com>
2023-08-25 23:16:44 +0300

    24 August's Task

---
+++ fourth_project/first_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, './first_app/home.html', {"name": "I am Rahim", "marks" : 86, "courses" : [
+        {
+            "id" : 1,
+            "couses" : "C",
+            "teacher" : "Rahim"
+        },
+        {
+            "id" : 2,
+            "couses" : "C++",
+            "teacher" : "kahim"
+        },
+        {
+            "id" : 3,
+            "courses" : "python",
+            "teacher" : "Fahim"
+        },
+    ]})
+
+def about(request):
+    return render(request, './first_app/about.html')
+    
+    
--- fourth_project/fourth_project/urls.py
+++ fourth_project/fourth_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for third_project project.
+URL configuration for fourth_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('first_app/', include('first_app.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('first_app/', include("first_app.urls")),
+]
